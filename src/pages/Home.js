7c9150ba143ba00412b9c682da7cff9b21352ad7
samstill <harshit842@yahoo.com>
2021-07-09 16:02:08 +0530

    added grid system to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,19 @@
-import React, { Component } from 'react'
-import Header from './components/Header'
+import React, { Component } from 'react';
+import "bootstrap/dist/css/bootstrap.css";
+import "./css/home.css";
+
 
 export default class Home extends Component {
     render() {
         return (
             <div>
-                <Header />
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col">
+                            
+                        </div>
+                    </div>
+                </div>
                 
             </div>
         )
